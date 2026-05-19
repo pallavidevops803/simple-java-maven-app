6832af185d71caadc6b9e515949930f4b461e6c5
pallavidevops803 <jamanchipallisaipallavi@gmail.com>
2026-05-19 17:31:31 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -2,11 +2,9 @@ package com.mycompany.app;
 
 public class App {
 
-    public String getMessage() {
-        return "Hello from Dockerized Java App!";
-    }
-
     public static void main(String[] args) {
+
         System.out.println("Hello from Dockerized Java App!");
+
     }
 }
